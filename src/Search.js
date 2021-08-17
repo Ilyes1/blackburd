6023b473698fdb0e2036efe66584bd44b3151c56
Ilyes1 <ihannouch7@gmail.com>
2021-08-17 20:02:36 +0100

    make it better

--- src/Search.js
+++ src/Search.js
@@ -7,98 +7,133 @@ function Search() {
     return (
         <div className="search">
 
-            <div className="search-images">
+            <div className="search-top">
+
+                <div className="video">
+                    <video controls></video>
+                    <ul>
+                        <li><i class="fas fa-times"></i></li>
+                        <li>63%</li>
+                        <li className="man"><i class="fas fa-male"></i></li>
+                        <li className="blue-circle"></li>
+                        <li className="blue-circle"></li>
+                    </ul>
+                </div>
 
                 <div className="image">
+                    <div className="image-top">
+                        <span className="detection">Image 1 of 20</span>
+                        <span className="download"><i class="fas fa-download"></i></span>
+                    </div>
                     <img src={image1} alt="" />
-                    <div>
-                        <span>OD-788</span>
-                        <ul>
-                            <li><i className="fas fa-download"></i></li>
-                            <li><i className="fas fa-camera"></i></li>
-                            <li><i className="fas fa-volume-up"></i></li>
-                            <li><i className="fas fa-exclamation-triangle"></i></li>
-                            <li className="live"><span>LIVE</span>.</li>
-                            <li><i className="fas fa-bars"></i></li>
-                        </ul>
-                    </div>
+                    <ul className="arrows">
+                        <li><i class="fas fa-arrow-left"></i> PREV</li>
+                        <li>NEXT <i class="fas fa-arrow-right"></i></li>
+                    </ul>
                 </div>
 
             </div>
 
-            <div className="bottom-images">
+            <ul className="bar">
 
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <li>Sowing 1 to 50 of 5634984, Ordered by time (Descending) in 5.15s</li>
+                <li><i class="fas fa-wifi"></i>Sensitivity (High) <i class="fas fa-sort-amount-up-alt"></i> Order by</li>
+                <li><i class="fas fa-arrow-left"></i> PREV  NEXT <i class="fas fa-arrow-right"></i></li>
+
+            </ul>
+
+            <div className="images">
+
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                <div className="bottom-image-container">
-                    <div className="bottom-image">
-                        <img src={image2} alt="" />
-                        <span>Person</span>
-                    </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
                 </div>
-                
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+                <div className="bottom-image">
+                    <img src={image2} />
+                    <span>Person</span>
+                </div>
+
             </div>
-
-        
-        
 
         </div>
     )
